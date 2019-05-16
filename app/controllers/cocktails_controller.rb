c9ab9a989328c4cd913d70805e874f2fd06370a8
Svitlana Piddubna <sv.piddubna@gmail.com>
2019-05-16 21:33:53 +0200

    Build main features

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,28 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+    @cocktail = Cocktail.new
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      redirect_to cocktails_path
+    end
+  end
+
+
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end

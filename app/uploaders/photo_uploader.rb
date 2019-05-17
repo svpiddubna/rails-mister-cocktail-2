4c5008aea2c24940be2647cf6f5840aa26787091
Svitlana Piddubna <sv.piddubna@gmail.com>
2019-05-17 11:05:25 +0200

    Add thumbnail photo to cocktails list

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,6 +1,10 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
+  version :bright_face do
+    cloudinary_transformation effect: "brightness:10", radius: 20,
+      width: 150, height: 150, crop: :thumb, gravity: :face
+  end
   # Remove everything else
 end
 
